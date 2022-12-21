2ccf0749f82ad6490f4438dad398dc0071035112
jorgevarios <jorgevarios@yahoo.com.ar>
2022-12-21 12:53:10 -0300

    subiendo archivos

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const path = require('path');
+app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static('public'));
+
+app.listen(3000, () =>{
+    console.log("servidor corriendo")
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+});
+
+
